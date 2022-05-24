1b04957d9898020520a8ae7eb0bf444e1499b095
viniciux <vinicius_q13@hotmail.com>
2022-05-24 18:46:59 -0300

    feat:adicionando indexController a rota index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
+const controller = require('../controllers/indexController')
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', controller.index);
 
 module.exports = router;
