65946998fd7fe2d9881e19adfaf900ad2da4de10
viniciux <vinicius_q13@hotmail.com>
2022-05-25 15:18:26 -0300

    feat: Adicionando convidados a view de convidados

--- server/controllers/convidadosController.js
+++ server/controllers/convidadosController.js
@@ -1,5 +1,6 @@
+const controller = require('../data/convidados.json')
 const controller = {}
 
-controller.index = (req, res) => res.render ('convidados',{title: 'Lista de Convidados'})
+controller.index = (req, res) => res.render ('convidados',{title: 'Lista de Convidados', convidados})
 
 module.exports = controller
